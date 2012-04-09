a774477ca61b083351de1c811cc9b9bf82be7dd6
jaycanuck <robitaille88@gmaill.com>
2012-04-09 12:19:04 -0500

    Added YqlpRequest and webOSExt

---
+++ yqlprequest/YqlpRequest.js
@@ -0,0 +1,16 @@
+enyo.kind({
+	name: "enyo.YqlpRequest",
+	kind: enyo.JsonpRequest,
+	published: {
+		query: ""
+	},
+	callbackName: "callback",
+	go: function(query) {
+		if(query) {
+			this.query = query;
+		}
+		this.url = "http://query.yahooapis.com/v1/public/yql?q=" + encodeURIComponent(this.query)
+				+ "'&format=json";
+		this.inherited(arguments);
+	}
+});

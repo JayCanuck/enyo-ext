9220c0f3e9ec6e7e3f823f99d46758f0f26e9ba1
Jason Robitaille <jason@canuckcoding.ca>
2013-02-12 09:07:42 -0600

    Win8Fix working

--- adaptiveimage/Win8Fix.js
+++ adaptiveimage/Win8Fix.js
@@ -1,8 +1,4 @@
-if(enyo.platform.ie>=10) {
-	var msViewportStyle = document.createElement("style");
-	msViewportStyle.appendChild(document.createTextNode("@-ms-viewport{width:auto!important}"));
-	document.getElementsByTagName("head")[0].
-	appendChild(msViewportStyle);
+if(enyo.platform.ie>=10 && !window.devicePixelRatio) {
 	window.devicePixelRatio = ((window.matchMedia("(orientation: portrait)").matches ? screen.width : screen.height)/document.documentElement.clientWidth);
 	document.addEventListener("orientationchange", function() {
 		window.devicePixelRatio = ((window.matchMedia("(orientation: portrait)").matches ? screen.width : screen.height)/document.documentElement.clientWidth);

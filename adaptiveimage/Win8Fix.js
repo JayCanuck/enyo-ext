391f266f172f5980a3ea1b25abe4fb9ebc836e04
Jason Robitaille <jason@canuckcoding.ca>
2013-02-12 09:25:40 -0600

    Win8Fix.j commenting added

--- adaptiveimage/Win8Fix.js
+++ adaptiveimage/Win8Fix.js
@@ -1,6 +1,18 @@
+/*
+ * Win8Fix.js		MIT License, Copyright 2013, Jason Robitaille www.canuckcoding.ca
+ *
+ * A fix for IE10, which sets the value of window.devicePixelRatio
+ * Due to the nature of IE10 changing zoom level, so long as the viewport is set to device-width,
+ * the devicePixelRatio can be recalculated as a ratio of the screen width and document width on
+ * the orientationchange event. Due to a "bug" in IE10, the screen width and height values don't
+ * swap during device orientation changes so additional portrait/landscape check is needed in the
+ * calculations.
+ */
+
 if(enyo.platform.ie>=10 && !window.devicePixelRatio) {
-	window.devicePixelRatio = ((window.matchMedia("(orientation: portrait)").matches ? screen.width : screen.height)/document.documentElement.clientWidth);
-	document.addEventListener("orientationchange", function() {
+	var refreshPixelRatio = function() {
 		window.devicePixelRatio = ((window.matchMedia("(orientation: portrait)").matches ? screen.width : screen.height)/document.documentElement.clientWidth);
-	}, false);
+	};
+	document.addEventListener("orientationchange", refreshPixelRatio, false);
+	refreshPixelRatio();
 }

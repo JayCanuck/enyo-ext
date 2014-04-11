6887883dd2405b9230ba57f7204611940273b7ed
Jason Robitaille <robitaille88@gmail.com>
2014-04-11 16:47:08 -0500

    Added a ViewStack which can dynamically push/pop/swap views

---
+++ altmvc/AppStack.js
@@ -0,0 +1,16 @@
+/**
+    A special kind of<a href="#enyo.ViewStack">enyo.ViewStack</a> designed to only be used once 
+    per application. Access to this object instance will bound to enyo.stack for global manipulation
+    of the stack.
+
+    A handy idea is to have your root application kind be a subkind of enyo.AppStack.
+*/
+
+enyo.kind({
+    name:"enyo.AppStack",
+    kind:"enyo.ViewStack",
+    create: function() {
+        enyo.stack = this;
+        this.inherited(arguments);
+    }
+});
